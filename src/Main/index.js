7c957c0eff45d44c6035141dfec655081255c1bc
lalalakolpachok <akolpakova91@gmail.com>
2017-11-07 21:22:43 +0300

    fix: project structure

---
+++ src/Main/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import styled from "styled-components";
+import PageWrapper from "../common/PageWrapper";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <PageWrapper>flflf</PageWrapper>
+      </main>
+    );
+  }
+}
+
+export default Main;
